5353985a83d87ee0afe254a142af0072133595c2
liuyuyu <liuyuyu2333@gmail.com>
2018-09-07 20:12:30 +0800

    fix build path

--- Vue.config.js
+++ Vue.config.js
@@ -2,7 +2,6 @@ module.exports = {
     // 基本路径
     baseUrl: '/which-name',
     outputDir: 'docs',
-    indexPath:'public/index.html',
     // 生产环境是否生成 sourceMap 文件
     productionSourceMap: false,
     // 服务器端口号
