7662b8436c6d2f4e5dc85a31de8c91df138df812
liuyuyu <liuyuyu2333@gmail.com>
2018-09-08 11:40:41 +0800

    fix github pages path

--- Vue.config.js
+++ Vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // 基本路径
-    baseUrl: process.env.NODE_ENV === 'production'?'/which-name':'/',
+    baseUrl: '/which-name',
     outputDir: 'docs',
     // 生产环境是否生成 sourceMap 文件
     productionSourceMap: false,
